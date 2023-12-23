f5cc627d68e22fd69b9742e83d01aa1a094cc768
sebastian <s.bohorquezd1@uniandes.edu.co>
2023-12-23 13:33:45 -0500

    configure tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 function Header() {
     return(
-        <h1>Desde header</h1>
+        <header>
+            <h1 className="text-4xl font-bold mt-3 uppercase">Desde header</h1>
+        </header>
     )
 }
 
